0c532ec7ab4fcc107282fe328470f7c8e70940c9
Emanuel Saramago <reg@esaramago.com>
2015-01-04 19:50:12 +0000

    homepage + ajustes

--- js/script.js
+++ js/script.js
@@ -1,38 +1,64 @@
 $(document).ready(function() {
-    // Remove default styles
-    $('link[rel="stylesheet"]').remove();
     
-    // Layout
-    //$('body').wrapInner('<div class="wrapper"></div>');
-    //$('#footer').appendTo('body');
-    
-    // Logo
-    var logo_origin = $('#siteSub').text();
-    var logo_text = logo_origin.substr(logo_origin.indexOf(" ") + 1);
-    $('.mw-wiki-logo').text(logo_text);
-    
-    //Language
-    var language = window.location.href.split("/")[2].split(".")[0].split("-")[0];
-    var $p_lang = $('#p-lang');
-    $('#p-lang-label').text(language).on('click', function() {
-        if ($p_lang.hasClass('active')) {
-            $p_lang.removeClass('active');
-        }
-        else {
-            $p_lang.addClass('active');
-        }
-    });
-    
-    // Menu
-    $('#toc a').on('click', function(e) {
-        e.preventDefault();
-        
-        var target = $(this).attr('href').substring(1);
-        var target_pos = document.getElementById(target).offsetTop;
-        $('html, body').animate({
-            scrollTop: target_pos - 100
+    try {
+        // Remove default styles
+        $('link[rel="stylesheet"]').remove();
+
+        // Layout
+        //$('body').wrapInner('<div class="wrapper"></div>');
+        //$('#footer').appendTo('body');
+
+        // Logo
+        var logo_origin = $('#siteSub').text();
+        var logo_text = logo_origin.substr(logo_origin.indexOf(" ") + 1);
+        $('.mw-wiki-logo').text(logo_text);
+
+        //Language
+        var language = window.location.href.split("/")[2].split(".")[0].split("-")[0];
+        var $p_lang = $('#p-lang');
+        $('#p-lang-label').text(language).on('click', function() {
+            if ($p_lang.hasClass('active')) {
+                $p_lang.removeClass('active');
+            }
+            else {
+                $p_lang.addClass('active');
+            }
         });
-    });
+
+        // Menu
+        $('#toc a').on('click', function(e) {
+            e.preventDefault();
+
+            var target = $(this).attr('href').substring(1);
+            var target_pos = document.getElementById(target).offsetTop;
+            $('html, body').animate({
+                scrollTop: target_pos - 100
+            });
+        });
+
+        // wikitables
+        if ($('.wikitable').length > 0) {
+            $('.wikitable').wrap('<div class="wikitable-wrap"></span>');
+        }
+        
+        // Show ===================================================================
+        $('html').addClass('show');
+
+
+        // Images
+        $('.thumbimage[srcset]').each(function() {
+            var $this = $(this);
+            var url = $this.attr('srcset').split('1.5x')[0];
+            $this.attr('src', url);
+        });
+    }
+    catch(err) {
+
+        // Show ===================================================================
+        $('html').addClass('show');
+    }
+
+
     
     // menu title
     var page_title = $('#firstHeading').find('span').text();
@@ -44,13 +70,4 @@ $(document).ready(function() {
             });
         })
     }, 400);
-    
-    if ($('.wikitable').length > 0) {
-        $('.wikitable').wrap('<div class="wikitable-wrap"></span>');
-    }
-    
-    // Show 
-    $('html').addClass('show');
 });
-
-
